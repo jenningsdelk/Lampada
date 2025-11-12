dda2b005e98fd8aa61eab904c4963bdfba555c58
Jennings Delk <jenningsdelk@hotmail.com>
2025-11-12 15:23:51 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,36 +1,7 @@
-function Ligar(){
-    document.getElementById('lampada').src='imgs/in.gif ';
+function Ligar() {
+    document.getElementById('lampada').src='imgs/pic_bulbon.gif';
 }
-function Desligar(){
-    document.getElementById('lampada').src='imgs/off.gif ';
-}
-function Ligar1(){
-    document.getElementById('lampada1').src='imgs/in.gif ';
-}
-function Desligar1(){
-    document.getElementById('lampada1').src='imgs/off.gif ';
-}
-function Ligar2(){
-    document.getElementById('lampada2').src='imgs/in.gif ';
-}
-function Desligar2(){
-    document.getElementById('lampada2').src='imgs/off.gif ';
-}
-function Ligar3(){
-    document.getElementById('lampada3').src='imgs/in.gif ';
-}
-function Desligar3(){
-    document.getElementById('lampada3').src='imgs/off.gif ';
-}
-function Ligar4(){
-    document.getElementById('lampada4').src='imgs/in.gif ';
-}
-function Desligar4(){
-    document.getElementById('lampada4').src='imgs/off.gif ';
-}
-function Feliz(){
-    document.getElementById('carinha').src='imgs/feliz.gif ';
-}
-function Triste(){
-    document.getElementById('carinha').src='imgs/triste.gif ';
+
+function Desligar() {
+    document.getElementById('lampada').src='imgs/pic_bulboff.gif';
 }
